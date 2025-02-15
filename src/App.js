37f22ec37fbea1246d8e0b83d712796d6dfefc56
henryt56 <henrytruong790@gmail.com>
2025-02-15 09:35:06 -0500

    Readded Authprovider

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
+    <AuthProvider>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,10 +20,11 @@ function App() {
           Learn React
         </a>
         <div>
-      Test div
-    </div>
+          Test div
+        </div>
       </header>
     </div>
+    </AuthProvider>
   );
 }
 
