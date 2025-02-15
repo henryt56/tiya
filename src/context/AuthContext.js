89c1458b775b15a68cad12eb4ffe6fd7bae6e8d7
henryt56 <henrytruong790@gmail.com>
2025-02-15 08:00:35 -0500

    Inital Firebase Setup

---
+++ src/context/AuthContext.js
@@ -0,0 +1,28 @@
+import { createContext, useState, useEffect } from "react";
+import { auth } from "../firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
+import { getUserRole } from "../services/authService";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [role, setRole] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+            setUser(currentUser);
+            if (currentUser) {
+                const userRole = await getUserRole(currentUser.uid);
+                setRole(userRole);
+            }
+        });
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user, role }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
---
+++ src/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDGF2Aht2AbzW0ZEe8xVWtIT2BLDnVIPRc",
+  authDomain: "tiya-tutoring.firebaseapp.com",
+  projectId: "tiya-tutoring",
+  storageBucket: "tiya-tutoring.firebasestorage.app",
+  messagingSenderId: "894845891073",
+  appId: "1:894845891073:web:4cb886ea4046792bd8c600",
+  measurementId: "G-1P4NFNHEYF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
