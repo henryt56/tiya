9144fc54988e806246f22f7464b88dfa48d81892
pocscz06 <158521706+pocscz06@users.noreply.github.com>
2025-02-20 17:35:47 -0500

    Created ReactRoute paths

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/Login';
+import RegisterPage from './pages/Register';
+import SearchPage from './pages/Search';
+import StudentDashboardPage from './pages/StudentDashboard';
+import TutorDashboardPage from './pages/TutorDashboard';
+import AdminDashboardPage from './pages/AdminDashboard';
+import TutorProfilePage from './pages/TutorProfile';
+import PaymentPage from './pages/Payments';
+import TransactionHistoryPage from './pages/TransactionHistory';
+import NotFoundPage from './pages/NotFoundPage';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <HomePage />,
+        errorElement: <NotFoundPage />
+    },
+    {
+        path: '/tutorprofile',
+        element: <TutorProfilePage />,
+        errorElement: <NotFoundPage />
+    },
+    {
+        path: '/admindashboard',
+        element: <AdminDashboardPage />,
+        errorElement: <NotFoundPage />
+    },
+    {
+        path: '/register',
+        element: <RegisterPage />,
+        errorElement: <NotFoundPage />
+    },
+    {
+        path: '/login',
+        element: <LoginPage />,
+        errorElement: <NotFoundPage />
+    },
+    {
+        path: '/payment',
+        element: <PaymentPage />,
+        errorElement: <NotFoundPage />
+    },
+    {
+        path: '/search',
+        element: <SearchPage />,
+        errorElement: <NotFoundPage />,
+    },
+    {
+        path: '/tutordashboard',
+        element: <TutorDashboardPage />,
+        errorElement: <NotFoundPage />
+    },
+    {
+        path: '/studentdashboard',
+        element: <StudentDashboardPage />,
+        errorElement: <NotFoundPage />
+    },
+    {
+        path: '/transactionhistory',
+        element: <TransactionHistoryPage />,
+        errorElement: <NotFoundPage />
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+)
