cb0dfde4d0c289d0befafcb54dff9441d1faca69
pocscz06 <158521706+pocscz06@users.noreply.github.com>
2025-03-05 14:35:00 -0500

    Fixed import

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import Link from "next/link";
 
 function NotFoundPage() {
-    return (
-       <div className="flex flex-col gap-2">
-        404 Not Found <br></br>
-        <Link to="/">Home</Link>
-       </div> 
-    );
+  return (
+    <div className="flex flex-col gap-2">
+      404 Not Found <br></br>
+      <Link to="/">Home</Link>
+    </div>
+  );
 }
 
 export default NotFoundPage;
