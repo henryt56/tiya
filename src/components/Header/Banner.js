d30627b1c1c30ecb82ac60f57f75dd57c35354ca
pocscz06 <158521706+pocscz06@users.noreply.github.com>
2025-03-05 12:17:02 -0500

    Created Header component w/ Banner & Logo; Navbar with tabs incomplete

---
+++ src/components/Header/Banner.js
@@ -0,0 +1,25 @@
+import styles from "./Banner.module.css";
+import { MdOutlineMessage } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
+import { FiMenu } from "react-icons/fi";
+
+export default function Banner() {
+  // WILL LATER IMPLEMENT FUNCTIONALITY TO ICON BUTTONS
+  return (
+    <div className={styles.banner}>
+      <div className={styles.bannerContent}></div>
+
+      <div className={styles.navIcons}>
+        <button className={styles.iconButton}>
+          <MdOutlineMessage />
+        </button>
+        <button className={styles.iconButton}>
+          <CgProfile />
+        </button>
+        <button className={styles.iconButton}>
+          <FiMenu />
+        </button>
+      </div>
+    </div>
+  );
+}
