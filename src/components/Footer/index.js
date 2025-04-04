49bc042824a7096dea5eebe56d24aedce7ccbd40
simone lattimore <simonee8420@gmail.com>
2025-04-03 21:16:51 -0400

    Update and recover footer background

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import { FaFacebookF, FaLinkedinIn, FaYoutube, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className={styles.tiyaFooter}>
-      <div className={styles.footerContainer}>
-        <p>&copy; {new Date().getFullYear()} Tutor in Your Area</p>
-        <div className={styles.footerLinks}>
-          <a href="/contact">Contact</a>
-          <a href="/about">About</a>
-          <a href="/privacy">Privacy</a>
+    <footer className={styles.footer}>
+      <div className={styles.footerTop}>
+        <div className={styles.logo}>TIYA</div>
+
+        <div className={styles.columns}>
+          <div>
+            <h4>Contact Us</h4>
+            <ul>
+              <li><a href="#">Careers</a></li>
+              <li><a href="#">Questions & Answers</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+            </ul>
+          </div>
+
+          <div>
+            <h4>Register</h4>
+            <ul>
+              <li><a href="#">Homework Help</a></li>
+              <li><a href="#">Test Prep</a></li>
+              <li><a href="#">Page</a></li>
+            </ul>
+          </div>
+
+          <div>
+            <h4>About Us</h4>
+            <ul>
+              <li><a href="#">Site Map</a></li>
+              <li><a href="#">Help Center</a></li>
+              <li><a href="#">Tips and Tricks</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.footerBottom}>
+        <div className={styles.socials}>
+          <FaFacebookF />
+          <FaLinkedinIn />
+          <FaYoutube />
+          <FaInstagram />
         </div>
       </div>
     </footer>
