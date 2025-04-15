c3c21cf8a924433364a66f628dae6317dd02e470
pocscz06 <158521706+pocscz06@users.noreply.github.com>
2025-04-14 22:17:10 -0400

    Fixed routing typo

--- src/services/login-register/Login.js
+++ src/services/login-register/Login.js
@@ -123,7 +123,7 @@ const Login = () => {
           <div className="d-flex justify-content-between align-items-center mb-3">
             <div>
               <Link
-                href="/forgot-password"
+                href="/Forgot-password"
                 className="text-decoration-none"
                 style={{ color: '#67c2ff' }}
               >
